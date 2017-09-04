9a1087cceca9594b7306698e4c011176573b5583
Bhavik Gosrani <bhavik4748@gmail.com>
2017-09-04 15:02:39 -0400

    added lazy loading

--- app/app.config.js
+++ app/app.config.js
@@ -1,21 +1,14 @@
 
 var app = angular.module('DemoApp', []);
 
-app.controller('DemoAppController', ['$scope', '$http', function ($scope, $http) {
-    $scope.test = "Hello World";
-
-    var sObj = {};
+app.controller('DemoAppController', ['$scope', '$http', function ($scope, $http) {   
+   
     $scope.propertyName = null;
     $scope.reverse = false;
-
-    $scope.redfonts = "redfonts";
-    $scope.sort = function (obj) {
-        //switch (obj) {
-        //    case 1: sObj.accSort = 1;
-
-        //}
-        //console.log(obj);
-        $scope.sortBy();
+    $scope.loadAll = 2;
+    $scope.redfonts = "redfonts"; 
+    $scope.loadAllFunc = function () {
+        $scope.loadAll = 1;
     }
 
     $scope.sortBy = function (propertyName) {
@@ -24,13 +17,22 @@ app.controller('DemoAppController', ['$scope', '$http', function ($scope, $http)
     };
 
     $http.get('/app/data.json').then(function (response) {
-        $scope.dataapi = response.data;
+        console.log(response.data.length);
+        //$scope.dataapi = [];
+        //var counter = 1;
+        //angular.forEach(response.data, function (item) {
+        //    $scope.dataapi.push(item);
+        //    ++counter;
+        //    if(counter > (response.data.length / 2) )
+        //        break;
+
+        //});
+       $scope.dataapi = response.data;
     }, function (reject) {
         return false;
     });
 
     $scope.checkValue = function (val) {
-
         if (val.percentChange == 0)
             return 'grayfonts';
         else if (val.percentChange > 0)
