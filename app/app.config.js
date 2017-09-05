7b040fb34ae220f711b587581412c39be4dd43ab
Bhavik Gosrani <bhavik4748@gmail.com>
2017-09-04 21:24:20 -0400

    added component and data service

--- app/app.config.js
+++ app/app.config.js
@@ -1,33 +1,67 @@
 
 var app = angular.module('DemoApp', []);
 
-app.controller('DemoAppController', ['$scope', '$http', function ($scope, $http) {   
-   
-    $scope.propertyName = null;
-    $scope.reverse = false;
-    $scope.loadAll = 2;
-    $scope.redfonts = "redfonts"; 
-    $scope.loadAllFunc = function () {
-        $scope.loadAll = 1;
-    }
+app.service('dataService', ['$http', '$q',  function ($http, $q) {        
+    this.makeHttpGetRequest = function (methodName) {
+        var deferred = $q.defer();
+        var req = {
+            method: 'GET',
+            url: '/app/data.json'           
+        }
 
-    $scope.sortBy = function (propertyName) {
-        $scope.reverse = ($scope.propertyName === propertyName) ? !$scope.reverse : false;
-        $scope.propertyName = propertyName;
+        try {
+            $http(req).then(function (response) {
+                if (response.status === 200)
+                    deferred.resolve(response);               
+            }, function (reject) {
+                // add logging service to track reject response
+                console.log(reject);
+                deferred.reject(reject);
+            });
+        }
+        catch (reject) {
+            // add logging service to track reject response
+            console.log(reject);
+            deferred.reject(reject);
+        }
+        return deferred.promise;
     };
 
-    $http.get('/app/data.json').then(function (response) {        
-       $scope.dataapi = response.data;
-    }, function (reject) {
-        return false;
-    });
-
-    $scope.checkValue = function (val) {
-        if (val.percentChange == 0)
-            return 'grayfonts';
-        else if (val.percentChange > 0)
-            return 'greenfonts';
-        else
-            return 'redfonts'
-    }
 }]);
+
+app.controller('DemoAppController', ['$scope',  function ($scope) {
+
+}]);
+
+app.component('demoComponent', {
+    templateUrl: 'app/demoComponent.html',
+    controller: ['$scope',  'dataService', function ($scope,  dataService) {
+        $scope.propertyName = null;
+        $scope.reverse = false;
+        $scope.loadAll = 2;
+        $scope.redfonts = "redfonts";
+        $scope.loadAllFunc = function () {
+            $scope.loadAll = 1;
+        }
+
+        $scope.sortBy = function (propertyName) {
+            $scope.reverse = ($scope.propertyName === propertyName) ? !$scope.reverse : false;
+            $scope.propertyName = propertyName;
+        };
+
+        dataService.makeHttpGetRequest().then(function (response) {
+            $scope.dataapi = response.data;
+        }, function (reject) {
+            return false;
+        });
+
+        $scope.checkValue = function (val) {
+            if (val.percentChange == 0)
+                return 'grayfonts';
+            else if (val.percentChange > 0)
+                return 'greenfonts';
+            else
+                return 'redfonts'
+        }
+    }]
+});
